36096b9993230a40b8c8852ff937ec0cb398a81a
Michael Walton <st0rmshadow@etherninja.org>
2019-10-29 20:48:13 -0500

    Removed More IFDEF Linux and FREEBSD

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -79,21 +79,10 @@ inline void *_rtw_zvmalloc(u32 sz)
 
 inline void _rtw_vmfree(void *pbuf, u32 sz)
 {
-#ifdef PLATFORM_LINUX
 	vfree(pbuf);
-#endif
-#ifdef PLATFORM_FREEBSD
-	free(pbuf, M_DEVBUF);
-#endif
-#ifdef PLATFORM_WINDOWS
-	NdisFreeMemory(pbuf, sz, 0);
-#endif
-
 #ifdef DBG_MEMORY_LEAK
-#ifdef PLATFORM_LINUX
 	atomic_dec(&_malloc_cnt);
 	atomic_sub(sz, &_malloc_size);
-#endif
 #endif /* DBG_MEMORY_LEAK */
 }
 
